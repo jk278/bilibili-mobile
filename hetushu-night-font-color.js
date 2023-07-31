1f0b9f9d07214150dd2c2258bcd7c9836c1d1c98
jk278 <g1169861954@gmail.com>
2023-07-31 21:13:36 +0800

    .

--- hetushu-night-font-color.js
+++ hetushu-night-font-color.js
@@ -16,34 +16,35 @@
 
     function executeAfterDOMContentLoaded(callback) {
         if (document.readyState === 'loading') {
-          document.addEventListener('DOMContentLoaded', () => callback());
+            document.addEventListener('DOMContentLoaded', () => callback());
         } else {
-          callback();
+            callback();
         }
-      }
+    }
 
     function addCssBeforeRendering() {
-        
+
         const style = document.createElement('style');
         style.textContent = `
-        body[data-mode="night"] #cbox #content { color: rgb(120, 132, 156); }
-        `;
-    
+          body[data-mode="night"] #cbox #content { color: rgb(120, 132, 156); }
+          `;
+        // 比例：10：11：13，倍数：12
+
         // 如果 document.documentElement 可用，将样式添加到文档
         if (document.documentElement) {
-          document.documentElement.appendChild(style);
+            document.documentElement.appendChild(style);
         } else {
-          // 如果 document.documentElement 不可用，监听 readyStateChange 事件
-          document.addEventListener('readystatechange', () => {
-            if (!style.isConnected && document.readyState !== 'loading') {
-              document.documentElement.appendChild(style);
-            }
-          });
+            // 如果 document.documentElement 不可用，监听 readyStateChange 事件
+            document.addEventListener('readystatechange', () => {
+                if (!style.isConnected && document.readyState !== 'loading') {
+                    document.documentElement.appendChild(style);
+                }
+            });
         }
     }
 
     executeAfterDOMContentLoaded(function () {
         addCssBeforeRendering();
-      });
+    });
 
 })();
