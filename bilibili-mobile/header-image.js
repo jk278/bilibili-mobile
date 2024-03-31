e1aa0cff7513db7141dbdb7b4064b18fcaf848de
jk278 <g1169861954@gmail.com>
2024-03-31 18:59:05 +0800

    .

---
+++ bilibili-mobile/header-image.js
@@ -0,0 +1,74 @@
+// 控制首页头图函数
+export function handleHeaderImage () {
+  const key = 'header-image'
+  const url = 'https://source.unsplash.com/random/840x400'
+  const elementSelector = '.bili-header__banner'
+
+  if (window.location.pathname === '/') {
+    loadImage(key, elementSelector)
+
+    setTimeout(async () => {
+      try {
+        const img = await getImage(url)
+        const base64Data = imageToBase64(img)
+        storeImage(key, base64Data)
+      } catch (error) {
+        console.error('Failed to get image:', error)
+      }
+    }, 5000)
+  }
+
+  function getImage (url) {
+    return new Promise((resolve, reject) => {
+      const img = new Image()
+      img.crossOrigin = 'Anonymous'
+      img.src = url
+      img.onload = () => resolve(img)
+      img.onerror = reject
+    })
+  }
+
+  function imageToBase64 (img) {
+    const canvas = document.createElement('canvas')
+    canvas.width = img.width
+    canvas.height = img.height
+    const ctx = canvas.getContext('2d')
+    ctx.drawImage(img, 0, 0)
+    return canvas.toDataURL('image/jpeg')
+  }
+
+  function storeImage (key, base64Data) {
+    localStorage.setItem(key, base64Data)
+  }
+
+  function loadImage (key, elementSelector) {
+    const base64Data = localStorage.getItem(key)
+    if (base64Data) {
+      const style = document.createElement('style')
+      style.innerHTML = `
+          ${elementSelector}::after {
+            content: '';
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            z-index: -1;
+            background-image: url(${base64Data});
+            background-size: cover;
+            background-position: center;
+          }
+        `
+      document.head.appendChild(style)
+    } else {
+      // 如果本地存储中不存在图片数据，则从 URL 中获取图片
+      getImage(url).then((img) => {
+        const base64Data = imageToBase64(img)
+        storeImage(key, base64Data)
+        loadImage(key, elementSelector)
+      }).catch((error) => {
+        console.error('Failed to get image:', error)
+      })
+    }
+  }
+}
---
+++ bilibili-mobile/setting.js
@@ -0,0 +1,157 @@
+import { waitDOMContentLoaded, ensureHeadGetted } from './app'
+
+// 脚本预加载设置
+export function handleScriptPreSetting () {
+  const defaultValue = [1, 1, 1, 1]
+
+  const css = {
+    css1: `
+      .bpx-player-sending-area.bpx-player-sending-area {display: none !important;}
+      .left-container.left-container {padding: calc(var(--video-height) + 5px ) 10px 0;}
+    `,
+    css2: '.main-reply-box.main-reply-box {display: none !important;}',
+    css3: '#v_tag {display: none !important;}',
+    css4: `
+      .copyright.item {display: none !important;}
+      .show-more {display: none;}`
+  }
+
+  readScriptSetting()
+
+  waitDOMContentLoaded(() => {
+    createSettingPanel()
+
+    // eslint-disable-next-line no-undef
+    GM_registerMenuCommand('元素隐藏设置', () => {
+      document.getElementById('setting-panel-style').classList.add('show')
+    })
+  })
+
+  function readScriptSetting (diference) {
+    diference = diference || false
+
+    const settingShowHidden = JSON.parse(localStorage.getItem('settingShowHidden')) || defaultValue
+    const values = Object.values(css)
+
+    if (diference) {
+      for (const [index, value] of diference.entries()) {
+        if (value) {
+          if (settingShowHidden[index]) {
+            const scriptPreStyle = Object.assign(document.createElement('style'), {
+              id: `script-pre-style-${index + 1}`,
+              textContent: css[`css${index + 1}`]
+            })
+            ensureHeadGetted(scriptPreStyle)
+          } else {
+            document.head
+              ? document.getElementById(`script-pre-style-${index + 1}`).remove()
+              : waitDOMContentLoaded(document.getElementById(`script-pre-style-${index + 1}`))
+          }
+        }
+      }
+    } else {
+      for (const [index, value] of values.entries()) {
+        if (settingShowHidden[index]) {
+          const scriptPreStyle = Object.assign(document.createElement('style'), {
+            id: `script-pre-style-${index + 1}`,
+            textContent: value
+          })
+          ensureHeadGetted(scriptPreStyle)
+        }
+      }
+    }
+  }
+
+  function createSettingPanel () {
+    const settingPanel = Object.assign(document.createElement('div'), {
+      id: 'setting-panel-style',
+      className: 'setting-panel',
+      innerHTML: `
+        <div class="setting-title">选择隐藏元素：</div>
+        <div class="setting-checkboxes">
+          <label><input type="checkbox" value="1"><span>弹幕行</span></label>
+          <label><input type="checkbox" value="2"><span>评论行</span></label>
+          <label><input type="checkbox" value="3"><span>标签块</span></label>
+          <label><input type="checkbox" value="4"><span>转载声明</span></label>
+        </div>
+        `
+    })
+
+    const settingConform = Object.assign(document.createElement('button'), {
+      class: 'setting-conform',
+      textContent: '确认'
+    })
+
+    const checkboxElements = settingPanel.querySelectorAll('.setting-checkboxes input[type="checkbox"]')
+    const oldValues = JSON.parse(localStorage.getItem('settingShowHidden')) || defaultValue
+    for (const [index, value] of oldValues.entries()) {
+      checkboxElements[index].checked = value
+    }
+
+    settingConform.addEventListener('click', () => {
+      const selectedValues = Array.from(checkboxElements).map((checkbox) => (checkbox.checked ? 1 : 0))
+
+      localStorage.setItem('settingShowHidden', JSON.stringify(selectedValues))
+      const difference = selectedValues.map((value, index) => (value === oldValues[index] ? 0 : 1))
+
+      readScriptSetting(difference)
+
+      settingPanel.classList.remove('show')
+    })
+
+    settingPanel.appendChild(settingConform)
+    document.body.appendChild(settingPanel)
+  }
+}
+
+// 脚本设置
+export function handleScriptSetting () {
+  const defaultValue = '0'
+
+  const keyValue = {
+    key1: 'full-unmuted'
+  }
+
+  waitDOMContentLoaded(() => {
+    createSettingPanel()
+
+    // eslint-disable-next-line no-undef
+    GM_registerMenuCommand('操作偏好设置', () => {
+      document.getElementById('setting-panel-preference').classList.add('show')
+    })
+  })
+
+  function createSettingPanel () {
+    const settingPanel = Object.assign(document.createElement('div'), {
+      id: 'setting-panel-preference',
+      className: 'setting-panel',
+      innerHTML: `
+        <div class="setting-title">选择操作偏好：</div>
+        <div class="setting-checkboxes">
+          <label><input type="checkbox" value="1"><span>使用底部全屏按钮播放和打开声音</span></label>
+        </div>
+        `
+    })
+
+    const settingConform = Object.assign(document.createElement('button'), {
+      class: 'setting-conform',
+      textContent: '确认'
+    })
+
+    const values = Object.values(keyValue)
+    const checkboxElements = settingPanel.querySelectorAll('.setting-checkboxes input[type="checkbox"]')
+    for (const [index, value] of values.entries()) {
+      checkboxElements[index].checked = (localStorage.getItem(value) || defaultValue) === '1'
+    }
+
+    settingConform.addEventListener('click', () => {
+      for (const [index, value] of values.entries()) {
+        localStorage.setItem(value, checkboxElements[index].checked ? '1' : '0')
+      }
+      settingPanel.classList.remove('show')
+    })
+
+    settingPanel.appendChild(settingConform)
+    document.body.appendChild(settingPanel)
+  }
+}
