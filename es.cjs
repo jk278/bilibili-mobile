e319454bc7aa9bba09f13984e21097321b756352
jk278 <g1169861954@gmail.com>
2024-09-06 01:38:28 +0800

    .

---
+++ es.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true
+    },
+    extends: 'standard',
+    overrides: [
+      {
+        env: {
+          node: true
+        },
+        files: [
+          '.eslintrc.{js,cjs}'
+        ],
+        parserOptions: {
+          sourceType: 'script'
+        }
+      }
+    ],
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+    },
+    rules: {
+      camelcase: [
+        'error',
+        {
+          allow: ['^up_mid$']
+        }
+      ]
+    },
+    ignorePatterns: ['node_modules/**/*.js', 'dist/**/*.js']
+  }
+  
