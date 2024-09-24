398aa8b3fd7d678a4cb355aab527ed97ee331b3e
jk278 <g1169861954@gmail.com>
2024-09-25 00:58:27 +0800

    5.0.5

--- src/setting.ts
+++ src/setting.ts
@@ -25,8 +25,9 @@ div.bili-live-card.is-rcmd {
   height: 70px;
 }
 div.recommended-container_floor-aside .container {
-  padding: 20px 0;
-  grid-gap: 20px !important;
+  padding: 15px 0;
+  grid-gap: 15px !important;
+  background-color: white;
 }
 picture.v-img.bili-video-card__cover, picture.v-img.bili-live-card__cover {
   border-radius: 0 !important;
