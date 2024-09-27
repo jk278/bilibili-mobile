8399b89074ca02103466891e872fee90b0759f7a
jk278 <g1169861954@gmail.com>
2024-09-27 16:46:23 +0800

    5.1.2

--- src/video.ts
+++ src/video.ts
@@ -192,6 +192,10 @@ function handlelVideoClick() {
   })
 
   function unmute() {
+    // 修复 VIA 解除静音视频暂停的问题
+    const video = document.querySelector('video') as HTMLVideoElement
+    video.play()
+
     video.muted = false
     if (video.volume === 0) {
       ;(
