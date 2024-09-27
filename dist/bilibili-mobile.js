c9e537962dc97b189d6de8a64aa47d0d380b9e0e
jk278 <g1169861954@gmail.com>
2024-09-27 17:19:47 +0800

    .

--- dist/bilibili-mobile.js
+++ dist/bilibili-mobile.js
@@ -3096,7 +3096,6 @@ div.bili-dyn-item-draw__avatar {
       if (video.volume === 0) {
         document.querySelector(".bpx-player-ctrl-muted-icon").click();
       }
-      video.play();
     }
     videoArea.addEventListener("touchstart", (event) => {
       event.stopPropagation();
--- src/video.ts
+++ src/video.ts
@@ -198,9 +198,6 @@ function handlelVideoClick() {
         document.querySelector('.bpx-player-ctrl-muted-icon') as HTMLElement
       ).click()
     }
-
-    // 修复 VIA 解除静音时视频暂停的问题
-    video.play()
   }
 
   // 阻止视频响应滑动侧边栏
