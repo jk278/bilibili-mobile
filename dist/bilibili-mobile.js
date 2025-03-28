7a12946f1f3acefe72fe2a6d56607541e608a2fa
jk278 <g1169861954@gmail.com>
2025-03-28 10:24:48 +0800

    chore: update version to 5.1.7.5 in dist and config files

--- dist/bilibili-mobile.js
+++ dist/bilibili-mobile.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         bilibili 移动端
 // @namespace    https://github.com/jk278/bilibili-mobile
-// @version      5.1.7.4
+// @version      5.1.7.5
 // @author       jk278
 // @description  Safari打开电脑模式，其它浏览器关闭电脑模式修改网站UA，获取舒适的移动端体验。
 // @license      MIT
